8b3e8c5d30db84488f46781e1640429d54a5e900
Nicolas Duarte <nickboytube0418@gmail.com>
2024-08-24 20:54:04 -0300

    Criado a parte de skills

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ const chk = document.getElementById('chk')
 const myImage1 = document.getElementById('myImage1')
 const myImage2 = document.getElementById('myImage2')
 const myImage3 = document.getElementById('myImage3')
+const myImage4 = document.getElementById('myImage4')
+const myLine1 = document.getElementById('myLine1')
+const myLine2 = document.getElementById('myLine2')
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark')
@@ -9,9 +12,15 @@ chk.addEventListener('change', () => {
         myImage1.src = '/images/LinkedinLigth.svg'
         myImage2.src = '/images/gitHubLigth.svg'
         myImage3.src = '/images/PersonLinesFillLigth.svg'
+        myImage4.src = '/images/javaLigth.svg'
+        myLine1.style.border = '1px solid #fff'
+        myLine2.style.border = '1px solid #fff'
     } else {
         myImage1.src = '/images/Linkedin.svg'
         myImage2.src = '/images/gitHub.svg'
         myImage3.src = '/images/PersonLinesFill.svg'
+        myImage4.src = '/images/java.svg'
+        myLine1.style.border = '1px solid #000'
+        myLine2.style.border = '1px solid #000'
     }
 })
