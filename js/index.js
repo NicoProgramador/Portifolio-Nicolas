a25ac2add790efff377f364a95c6026866018b5c
Nicolas Duarte <nickboytube0418@gmail.com>
2024-08-25 18:02:58 -0300

    Corrigido mais uma vez as imagens no JavaScript

--- js/index.js
+++ js/index.js
@@ -9,17 +9,17 @@ const myLine2 = document.getElementById('myLine2')
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark')
     if (chk.checked) {
-        myImage1.src = '../images/LinkedinLigth.svg'
-        myImage2.src = '../images/gitHubLigth.svg'
-        myImage3.src = '../images/PersonLinesFillLigth.svg'
-        myImage4.src = '../images/javaLigth.svg'
+        myImage1.src = './images/LinkedinLigth.svg'
+        myImage2.src = './images/gitHubLigth.svg'
+        myImage3.src = './images/PersonLinesFillLigth.svg'
+        myImage4.src = './images/javaLigth.svg'
         myLine1.style.border = '1px solid #fff'
         myLine2.style.border = '1px solid #fff'
     } else {
-        myImage1.src = '../images/Linkedin.svg'
-        myImage2.src = '../images/gitHub.svg'
-        myImage3.src = '../images/PersonLinesFill.svg'
-        myImage4.src = '../images/java.svg'
+        myImage1.src = './images/Linkedin.svg'
+        myImage2.src = './images/gitHub.svg'
+        myImage3.src = './images/PersonLinesFill.svg'
+        myImage4.src = './images/java.svg'
         myLine1.style.border = '1px solid #000'
         myLine2.style.border = '1px solid #000'
     }
